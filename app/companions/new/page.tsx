533bbb52f6524784943116dae66c7c1dcbc2b8f8
Nandanikadave <nandani.kadave@somaiya.edu>
2025-07-11 16:13:40 +0530

    add companion builder form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import CompanionForm from "@/components/custom/CompanionForm"
 
 const NewCompanion = () => {
   return (
-    <div>
-      <h1>New Companion</h1>
-    </div>
+    <main className="items-center justify-center min-md:w-2/3 md:w-2/3 ">
+      <article className="w-full gap-4 flex flex-col">
+        <h1 className="mb-5">Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
