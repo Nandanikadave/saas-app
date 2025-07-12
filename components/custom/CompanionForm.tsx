f8ad5bc6ffc3e7cadf4d449108aca65329b04b8a
Nandanikadave <nandani.kadave@somaiya.edu>
2025-07-12 23:31:47 +0530

    implement supabase for companion form

--- components/custom/CompanionForm.tsx
+++ components/custom/CompanionForm.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 import {
   Form,
   FormControl,
@@ -47,9 +49,17 @@ const CompanionForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const companion = await createCompanion(values);
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    }
+    else {
+      console.log("Failed to create companion");
+      redirect('/');
+    }
   };
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
